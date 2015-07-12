51fb287a70d2431dad4ff7c4e6a02212e90d3381
Christopher <chiggiechang@gmail.com>
2015-07-12 16:19:39 -0700

    Testing out even more exports.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
 #[macro_use]
-#[macro_export]
-pub mod accessor;
+pub mod accessor {
+    #[macro_export]
+    macro_rules! getter {
+        ( $name:ident, $name_type:ident ) => {
+            pub fn $name(&self) -> $name_type {
+                self.$name
+            }
+        }
+    }
+
+    #[macro_export]
+    macro_rules! field_getter {
+        ( $($name:ident, $name_type:ident),+ ) => {
+            $(
+                getter!($name, $name_type);
+                )+
+        }
+    }
+
+    #[macro_export]
+    macro_rules! setter {
+        ( $name:ident, $set_name:ident, $name_type:ident ) => {
+            pub fn $set_name(&mut self, to_set: $name_type) {
+                self.$name = to_set;
+            }
+        }
+    }
+
+    #[macro_export]
+    macro_rules! field_setter {
+        ( $($name:ident, $set_name:ident, $name_type:ident ),+ ) => {
+            $(
+                setter!($ident, $set_name, $name_type);
+                )+
+        }
+    }
+
+    #[macro_export]
+    macro_rules! ref_getter {
+        ( $name:ident, $name_type:ident ) => {
+            pub fn $name(&self) -> &$name_type {
+                &self.$name
+            }
+        }
+    }
+}
